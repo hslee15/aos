8cf13bdcbd415f55f65156de8370adad2b286301
hslee15 <lhs01019@kbu.ac.kr>
2025-08-07 17:24:45 +0900

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React,{useEffect} from 'react'
 import './style/Hero.css'
+import image1 from '../assets/1.jpg'
+import image2 from '../assets/2.jpg'
+import image3 from '../assets/3.jpg'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
@@ -23,6 +26,11 @@ const Hero = () => {
         <p data-aos='fade-up' data-aos-delay="100">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, voluptas!
         </p>
+        <div className='image-container'>
+          <img className='img1' src={image1} alt="hero background" data-aos='fade-up' data-aos-delay="250"/>
+          <img className='img2' src={image2} alt="hero background" data-aos='fade-up' data-aos-delay="350"/>
+          <img className='img3' src={image3} alt="hero background" data-aos='fade-up' data-aos-delay="550"/>
+        </div>
       </div>
     </section>
   )
